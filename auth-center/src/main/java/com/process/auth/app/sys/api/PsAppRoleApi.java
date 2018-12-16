389fafbbb7a6b6f58f37898ca08019749061b919
danfeng <danfeng.diao@elitesland.com>
2018-12-16 21:27:02 +0800

    role develop

---
+++ auth-center/src/main/java/com/process/auth/app/sys/api/PsAppRoleApi.java
@@ -0,0 +1,49 @@
+package com.process.auth.app.sys.api;
+
+import com.process.auth.app.sys.entity.PsAppRoleEntity;
+import com.process.auth.app.sys.mapper.PsAppRoleMapper;
+import com.process.auth.app.sys.service.PsAppRoleService;
+import com.process.common.database.domain.PagingResult;
+import com.process.common.util.WebUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+
+/**
+ * @author Danfeng
+ * @since 2018/12/16
+ */
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class PsAppRoleApi {
+    private final PsAppRoleMapper appRoleMapper;
+    private final PsAppRoleService appRoleService;
+
+    @GetMapping("/roles")
+    public PagingResult<PsAppRoleEntity> pages(PsAppRoleQuery query) {
+        val total = appRoleMapper.total(query);
+        if (total > 0) {
+            return PagingResult.of(appRoleMapper.pages(query), total);
+        }
+        return PagingResult.empty();
+    }
+
+    @PutMapping("/role")
+    public ResponseEntity user(@RequestBody PsAppRoleEntity appRoleEntity) {
+        return WebUtil.ok(appRoleService.optEntity(appRoleEntity));
+    }
+
+    @GetMapping("/roles/{id}")
+    public ResponseEntity user(@PathVariable("id") long id) {
+        return WebUtil.ok(appRoleMapper.getEntity(id));
+    }
+
+    @DeleteMapping("/roles")
+    public ResponseEntity users(@RequestBody Long[] ids) {
+        return WebUtil.ok(appRoleService.batchDelete(Arrays.asList(ids)));
+    }
+}
